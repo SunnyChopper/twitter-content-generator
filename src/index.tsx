2248113cd11956d4b9821138b352242526fe0876
SunnyChopper <ishy.singh@gmail.com>
2023-07-19 20:42:45 -0500

    Introducing Redux
    Introducting Redux into the application for global state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import './index.css';
 import App from './App';
+import authReducer from "./authSlice";
 import reportWebVitals from './reportWebVitals';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
