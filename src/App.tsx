d2ad0a9b57337a0578c34d85d6b195863f2ad336
SunnyChopper <ishy.singh@gmail.com>
2023-07-19 20:45:00 -0500

    Integrate components
    This commit puts everything together into the `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Auth, Amplify } from "aws-amplify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import CircularProgress from "@mui/material/CircularProgress";
+
+import GenerateContentPage from "./components/5 - pages/GenerateContentPage";
+import UploadedFilesPage from "./components/5 - pages/UploadedFilesPage";
+import UserDashboardPage from "./components/5 - pages/UserDashboardPage";
+import PublicHomepage from "./components/5 - pages/PublicHomepage";
+import AvatarsPage from "./components/5 - pages/AvatarsPage";
+import LoginPage from "./components/5 - pages/LoginPage";
+
+import { setToken } from "./authSlice";
+
+Amplify.configure({
+	region: 'us-east-1',
+	userPoolId: 'us-east-1_3gSSU4ERq',
+	userPoolWebClientId: '56iajbie5m6fvd2sl5qb2rbfqa'
+});
+
+const App = () => {
+	const [isLoading, setIsLoading] = React.useState(true);
+
+	const dispatch = useDispatch();
+	const hasValidToken = useSelector((state: any) => state.auth.token !== null);
+
+	React.useEffect(() => { 
+		checkAuth();
+	}, []);
+
+	const checkAuth = async () => { 
+		try {
+			const session = await Auth.currentSession();
+			const token = session.getIdToken().getJwtToken();
+			dispatch(setToken(token));
+		} catch (error: any) {
+			dispatch(setToken(null));
+		}
+
+		setIsLoading(false);
+	}
+
+	if (isLoading) {
+		return (
+			<div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+				<CircularProgress />
+			</div>
+		);
+	}
+
+	return (
+		<div style={{ marginBottom: "100px" }}>
+			<Routes>
+				<Route path="/" element={<PublicHomepage />} />
+				<Route path="/login" element={<LoginPage />} />
+				{hasValidToken ? (
+					<>
+						<Route path="/dashboard" element={<UserDashboardPage />} />
+						<Route path="/generate" element={<GenerateContentPage />} />
+						<Route path="/avatars" element={<AvatarsPage />} />
+						<Route path="/files" element={<UploadedFilesPage />} />
+					</>
+				) : (
+					<Route path="*" element={<LoginPage />} />
+				)}
+			</Routes>
+		</div>
+	);
+};
 
 export default App;
