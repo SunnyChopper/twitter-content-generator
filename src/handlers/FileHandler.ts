fd7cdc62d2134050e19bc792f6bb01efedb837ce
SunnyChopper <ishy.singh@gmail.com>
2023-07-28 08:27:02 -0500

    Updated backend logic and naming
    Based on earlier changes made to naming, updated the code to match. Also fixed some bugs and implemented the logic to delete a file from the database.

--- src/handlers/FileHandler.ts
+++ src/handlers/FileHandler.ts
@@ -11,6 +11,21 @@ import { buildResponse } from 'src/utils/responses';
 // AWS Lambda
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
+export const createFileHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    if (event.body === null || event.body === undefined) {
+        return buildResponse(event, 400, { error: "Invalid request body. Cannot upload file." });
+    }
+
+    const fileService = new FileService();
+    try {
+        let uploadedFileId: number = await fileService.saveFile(JSON.parse(event.body) as TwitterFile);
+        return buildResponse(event, 200, { id: uploadedFileId });
+    } catch (error) {
+        console.error(error);
+        return buildResponse(event, 500, { error: `Failed to upload file. Error: ${error}` });
+    }
+}
+
 export const getFilesHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const userId = getCurrentUserId(event);
     if (userId === undefined) {
@@ -27,28 +42,17 @@ export const getFilesHandler = async (event: APIGatewayProxyEvent): Promise<APIG
     }
 }
 
-export const uploadFileHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    if (event.body === null || event.body === undefined) {
-        return buildResponse(event, 400, { error: "Invalid request body. Cannot upload file." });
-    }
-
-    const fileService = new FileService();
-    try {
-        let uploadedFileId: number = await fileService.saveFile(JSON.parse(event.body) as TwitterFile);
-        return buildResponse(event, 200, { id: uploadedFileId });
-    } catch (error) {
-        console.error(error);
-        return buildResponse(event, 500, { error: `Failed to upload file. Error: ${error}` });
-    }
-}
-
 export const deleteFileHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    if (event.body === null || event.body === undefined) {
+        return buildResponse(event, 400, { error: "Invalid request body. Cannot delete file." });
+    }
+
     let userId = getCurrentUserId(event);
     if (userId === undefined) {
         return buildResponse(event, 400, { error: "Invalid request body. Cannot retrieve an authorized user." });
     }
 
-    let fileId: string | undefined = event.pathParameters?.fileId;
+    let fileId: string | undefined = JSON.parse(event.body).fileId;
     if (fileId === null || fileId === undefined) {
         return buildResponse(event, 400, { error: "Invalid request body. Cannot delete file." });
     }
